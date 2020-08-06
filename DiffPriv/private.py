04a96c49e6a60e346e367615db32b54ab8066fa7
q9i <46249765+quantum9Innovation@users.noreply.github.com>
2020-08-05 17:51:56 -0700

    Added laplace mechanism

--- DiffPriv/private.py
+++ DiffPriv/private.py
@@ -1,4 +1,5 @@
 from . import np
+from . import random
 
 def random(response_list):
     """
@@ -24,3 +25,51 @@ def random(response_list):
 
             if b1 == 1:
                 response_list[i] = 1
+                
+def lapmech(data, file_name, epsilon, f, sample_size=10, delta_f=None):
+
+    """
+    It does what it says it does.
+    For more information, see https://www.github.com/quantum9innovation/Differential-Privacy/Wiki
+    Gracias
+    """
+    
+    new_data = open(file_name, 'w+')
+    raw_data = data.readlines()
+
+    rows = len(raw_data)
+    sample_size = min(rows, sample_size)
+    columns = len(raw_data[0].split(','))
+
+    if delta_f is None:
+
+        delta_f = []
+        data_draft = []
+
+        for c in range(columns):
+
+            data_draft.append([float(raw_data[r].split(',')[c]) for r in range(rows)])
+            samples = [int(rows * random.random()) for i in range(sample_size)]
+            delta_f.append(0.01)
+
+            for i in samples:
+
+                x_prime = [x for x in data_draft[-1]]
+                x_prime[i] += 1
+                delta_f[-1] = max(delta_f[-1], abs(f(data_draft[-1])-f(x_prime)))
+
+    for r in range(rows):
+
+        line = []
+        for c in range(columns):
+
+            b = delta_f[c]/epsilon
+            coin_flip = round(random.random())
+            if coin_flip == 0: line.append(float(raw_data[r].split(',')[c]) + random.expovariate(1 / (2 * b)))
+            if coin_flip == 1: line.append(float(raw_data[r].split(',')[c]) - random.expovariate(1 / (2 * b)))
+
+        new_data.writelines(str(line)+'\n')
+
+    return new_data
+
+               
