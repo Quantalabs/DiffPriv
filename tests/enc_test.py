9f50c60e9fac6ef94ab7852ce4588966ab75fe32
Quantalabs <55121845+Quantalabs@users.noreply.github.com>
2021-04-25 18:18:28 -0700

    Fix tests

--- tests/enc_test.py
+++ tests/enc_test.py
@@ -11,7 +11,7 @@ def test_rcipher ():
     assert enc.reverse_cipher('Test string') == 'gnirts tseT'
 
 def test_lfl ():
-    assert enc.lfl('Text', [['t', 'h'], ['e', 'i'], ['x', 'g'], ['T', 'H']]) == 'high'
+    assert enc.lfl('Text', [['t', 'h'], ['e', 'i'], ['x', 'g'], ['T', 'H']]) == 'High'
 
 def test_dlfl ():
     assert enc.dec_lfl('High', [['t', 'h'], ['e', 'i'], ['x', 'g'], ['T', 'H']]) == 'Text'
