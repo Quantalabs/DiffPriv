8f33b539e89711ffa237d82ec2086ced5ec1c1f4
Quantalabs <55121845+Quantalabs@users.noreply.github.com>
2021-05-16 13:27:17 -0700

    Prepare for v1.0.0-alpha release

--- setup.py
+++ setup.py
@@ -5,18 +5,13 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="DiffPriv",
-    version="0.0.2",
+    version="v1.0.0-alpha",
     author="Quantalabs",
     description="A Differential Privacy Package",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Quantalabs/DiffPriv",
     packages=setuptools.find_packages(),
-    project_urls={
-        "Bug Tracker": "https://github.com/Quantalabs/DiffPriv/issues",
-        "Documentation": "https://diffpriv.readthedocs.io",
-        "Source Code": "https://github.com/Quantalabs/DiffPriv"
-    },
     entry_points={"console_scripts": ["diffpriv=DiffPriv.cli:run"]},
     classifiers=[
         "Programming Language :: Python :: 3",
