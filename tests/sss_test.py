8b7bdbbce5f800dcbb56b49aefa9ff389d92a53a
QLabs <55121845+Quantalabs@users.noreply.github.com>
2021-05-21 21:28:50 -0700

    Update sss_test.py

--- tests/sss_test.py
+++ tests/sss_test.py
@@ -1,3 +1,10 @@
+import os
+import sys
+import csv
+
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
 
 import DiffPriv as diff
 
