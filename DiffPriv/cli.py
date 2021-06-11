7f489b02806b182480e3b3d4ca53f65efadb1aee
Quantalabs <55121845+Quantalabs@users.noreply.github.com>
2021-06-11 16:01:07 -0700

    Remove formatting for CLI
    Remove formatting for CLI for better readability.

--- DiffPriv/cli.py
+++ DiffPriv/cli.py
@@ -13,7 +13,7 @@ from . import webbrowser
 def lfl(enc_file, key_file, output):
     """lfl
 
-    \33[1mdiffpriv lfl input_file key_file outputfile\33[9m[optional]
+    diffpriv lfl input_file key_file outputfile[optional]
 
 The lfl command will encrypt the given input file with the given key and put the output in the given output file, or,
 if there is none, will automatically put the output in the input file, replacing the content.
@@ -29,7 +29,7 @@ if there is none, will automatically put the output in the input file, replacing
 def dlfl(denc_file, key_file, output):
     """dlfl
 
-    \33[1mdiffpriv dlfl input_file key_file outputfile\33[0m[optional]
+    diffpriv dlfl input_file key_file outputfile[optional]
 
 The dlfl command decrypts the given input file with the given key and put the output in the given output file, or,
 if there is none, will automatically put the output in the input file, replacing the content.
@@ -45,7 +45,7 @@ if there is none, will automatically put the output in the input file, replacing
 def rcipher(file, output):
     """rcipher
     
-    \33[1mdiffpriv rcipher file outputfile\33[0m[optional]
+    diffpriv rcipher file outputfile[optional]
 
 The rcipher command will run the reverse cipher on a given file and put the output in the given output file, or,
 if there is none, will automatically put the output in the input file, replacing the content.
@@ -58,11 +58,11 @@ if there is none, will automatically put the output in the input file, replacing
 def porta(ifile, ofile, key):
     """porta
 
-    \33[1mdiffpriv porta input_file output_file key\33[0m
+    diffpriv porta input_file output_file key
 
-The porta commmand will encrypt the message in the given input file using the given key and put the output in the given output file.\33[31m
-NOTE\33[0m: The key should not be part of a file, but passed in as text in the CLI. Use quotes for a multi-word key, ex \33[1m"This is a multi-word key"\33[0m
-or otherwise just \33[1mkey\33[0m for a single word key.
+The porta commmand will encrypt the message in the given input file using the given key and put the output in the given output file.
+NOTE: The key should not be part of a file, but passed in as text in the CLI. Use quotes for a multi-word key, ex "This is a multi-word key"
+or otherwise just key for a single word key.
 """
     cipher = enc.Porta()
 
@@ -81,11 +81,11 @@ or otherwise just \33[1mkey\33[0m for a single word key.
 def portadec(ifile, ofile, key):
     """portadec
 
-    \33[1mdiffpriv portadec input_file output_file key\33[0m
+    diffpriv portadec input_file output_file key
 
-The portadec command will decrypt a message in the input file using the given key and put the output in the given output file. \33[31m 
-NOTE\33[0m: The key should not be part of a file, but passed in as text in the CLI. Use quotes for a multi-word key, ex. \33[1m"This is a multi-word key"\33[0m
-or otherwise just \33[1mkey\33[0m for a single word key.
+The portadec command will decrypt a message in the input file using the given key and put the output in the given output file.  
+NOTE: The key should not be part of a file, but passed in as text in the CLI. Use quotes for a multi-word key, ex. "This is a multi-word key"
+or otherwise just key for a single word key.
 """
     cipher = enc.Porta()
 
@@ -103,25 +103,25 @@ or otherwise just \33[1mkey\33[0m for a single word key.
     
 def help(command=None):
     if command is None:
-        print('\33[1mDiffPriv CLI Help\n\33[0m')
+        print('DiffPriv CLI Help\n')
         print('The DiffPriv CLI provides an easy way to encrypt files or use differential privacy on data quickly and easily. See documentation for more detailed help - https://diffpriv.rtfd.io')
         print('\n \nCOMMANDS')
         print('---------------------------------------\n')
-        print('\33[31m--help \33[0m')
+        print('--help ')
         print('This command. Outputs help for the CLI. Add the name of a command after it for easy \n')
-        print('\33[31mlfl\33[0m')
-        print('Uses letter for letter encryption on a file. Format is \33[1mdiffpriv lfl input_file key_file outputfile\33[0m[optional] If last option is left blank, replaces the encryption file content with the new content\n')
-        print('\33[31mdlfl\33[0m')
-        print('Decrypts a file with letter for letter encryption. Format is \33[1mdiffpriv dlfl input_file key_file outputfile\33[0m[optional] If last option is left blank, replaces the decryption file content with the new content\n')
-        print('\33[31mrcipher\33[0m')
-        print('Reverse cipher. Format is \33[1mdiffpriv rcipher input_file output_fle\33[0m[optional] If last option is left blank, replaces the content of the input file with the encrypted text')
-        print('\n\33[31mporta\33[0m')
-        print('Encrypts file with the porta cipher. Format is \33[1mdiffpriv porta input_file output_file key\33[0m Last option is passed in as text not a file. Ex. \33[1mdiffpriv porta file.txt output.txt thisisthekey\33[0m')
-        print('\n\33[31mportadec\33[0m')
-        print('Decrypts file with the porta cipher. Format is \33[1mdiffpriv portadec input_file output_file key\33[0m Last option is passed in as text not a file. Ex. \33[1mdiffpriv portadec file.txt output.txt thisisthekey\33[0m')
-        print('\n\33[31m--docs\33[0m')
+        print('lfl')
+        print('Uses letter for letter encryption on a file. Format is \ndiffpriv lfl input_file key_file outputfile[optional] \nIf last option is left blank, replaces the encryption file content with the new content\n')
+        print('dlfl')
+        print('Decrypts a file with letter for letter encryption. Format is \ndiffpriv dlfl input_file key_file outputfile[optional]\n If last option is left blank, replaces the decryption file content with the new content\n')
+        print('rcipher')
+        print('Reverse cipher. Format is \ndiffpriv rcipher input_file output_fle[optional]  \nIf last option is left blank, replaces the content of the input file with the encrypted text')
+        print('\nporta')
+        print('Encrypts file with the porta cipher. Format is \ndiffpriv porta input_file output_file key\n Last option is passed in as text not a file. Ex. diffpriv porta file.txt output.txt thisisthekey')
+        print('\nportadec')
+        print('Decrypts file with the porta cipher. Format is \ndiffpriv portadec input_file output_file key\n Last option is passed in as text not a file. Ex. diffpriv portadec file.txt output.txt thisisthekey')
+        print('\n--docs')
         print('Shows diffpriv documentation for package or submodule.')
-        print('\n\33[31m--changelog\33[0m')
+        print('\n--changelog')
         print('Shows the changelog.')
     else:
         eval('print('+command+'.__doc__)') # skipcq: PYL-W0123
@@ -129,7 +129,7 @@ def help(command=None):
 def docs(submodule=None):
     """docs
     
-    \33[1mdiffpriv docs submodule\33[0moptional
+    diffpriv docs submodule[optional]
 
 The `--docs` command will open up the diffpriv documentation. If you pass in a submodule it will open up the docs for that submodule, eg. `--docs diff` will open up docs
 for the `diff` submodule.
